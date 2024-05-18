8dca2bd5d43fc7635e050fb53ec8a89e5769264b
gnuhub <gnuhub@qq.com>
2024-05-18 16:03:11 +0800

    build 10004-spring-maven-koylin

---
+++ 1.ci.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+ls -al
+cd demo
+echo "hello world"
+
+
+mvn  install
+
+ls -al
+cd target
+
+cp demo-0.0.1-SNAPSHOT.jar ~/
+file ~demo-0.0.1-SNAPSHOT.jar
+
+
